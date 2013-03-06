41712cc0a45624ca1c714d77bba8ea535ccc9a20
JohnMorales <jmorales@gmail.com>
2013-03-06 18:08:33 -0500

    Initial Commit

---
+++ Homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HomepwnerAppDelegate.h
+//  Homepwner
+//
+//  Created by John Morales on 3/6/13.
+//  Copyright (c) 2013 John Morales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
